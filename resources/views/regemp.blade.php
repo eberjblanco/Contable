fdb4285a97620820d13247189e9329baf816a0b3
eberjblanco <35938768+eberjblanco@users.noreply.github.com>
2020-07-14 11:14:59 -0400

    regemp

--- resources/views/regemp.blade.php
+++ resources/views/regemp.blade.php
@@ -22,134 +22,113 @@
         <div class="card-header">
           <h3 class="card-title">Registrar Nueva Empresa</h3>
         </div>        
-        <form role="form" action="{{ route('regemp.store')}}" method="POST" name="f1">
+        <form role="form" action="{{ route('regemp.store')}}" method="POST" name="f1" enctype="multipart/form-data">
           @csrf
           <div class="card-body">
-  <div class="row">
-    <div class="col">
-            <div class="form-group">
-              <label for="exampleInputEmail1">Nit</label>
-              <input required="" name="Nit_nva" type="text" class="form-control" id="Nit_nva" placeholder="Ingrese Nit">
-            </div>
-    </div>        
-    <div class="col">
-            <div class="form-group col-md-6">
-              <label for="exampleInputPassword1">Razón</label>
-              <input required="" name="Razon_nva" type="text" class="form-control" id="Razon_nva" placeholder="Ingrese Razón">
-            </div>
-  </div>
-</div>
-
-
-
-<!--Departamento -->
-<div class="row">
-    <div class="col">
-      <div class="form-group">
-                <div class="form-group">
-                <label for="exampleInputPassword1">Departamento</label>
-                <input required="" name="Departamento_nva" type="text" class="form-control" id="Departamento_nva" placeholder="Ingrese Departamento">
+            <div class="row">
+              <div class="col-md-4">
+              <div class="row">
+                <div class="col-md-12">
+                  <label>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</label>
                 </div>
-      </div>
-    </div>
-<!--Municipio -->
-    <div class="form-group col-md-6">
-          <div class="col">
-            <div class="form-group">
-              <label for="exampleInputPassword1">Municipio</label>
-              <input required="" name="Municipio_nva" type="text" class="form-control" id="Municipio_nva" placeholder="Ingrese Municipio">
-            </div>
-          </div>
-    </div>
-</div>
-                          <!-- Nuevos Campos insertados -->
-                          <!-- Departamento -->
-<!--
-    <div class="form-group col-md-12">
-    <label for="exampleInputPassword1">Departamento</label>
-            <select class="form-control" id="pais" name=Departamento_Nvo onchange="cambia_provincia()"> 
-            <option selected value="0">Seleccione Departamento
-            <option value="1">Amazonas
-            <option value="2">Antioquia
-            <option value="3">Arauca
-            <option value="4">Atlántico
-            <option value="5">Bolívar
-            <option value="6">Boyacá
-            <option value="7">Caldas
-            <option value="8">Caquetá
-            <option value="9">Casanare
-            <option value="10">Cauca
-            <option value="11">Cesar
-            <option value="12">Chocó
-            <option value="13">Córdoba
-            <option value="14">Cundinamarca
-            <option value="15">Guainía
-            <option value="16">Guaviare
-            <option value="17">Huila
-            <option value="18">La Guajira
-            <option value="19">Magdalena
-            <option value="20">Meta
-            <option value="21">Nariño
-            <option value="22">Norte de Santander
-            <option value="23">Putumayo
-            <option value="24">Quindío
-            <option value="25">Risaralda
-            <option value="26">San Andrés y Providencia
-            <option value="27">Santander
-            <option value="28">Sucre
-            <option value="29">Tolima
-            <option value="30">Valle del Cauca
-            <option value="31">Vaupés
-            <option value="32">Vichada
-            </select>
-    </div>
-    <div class="form-group col-md-12">
-    <label for="exampleInputPassword1">Municipio</label>
-            <select class="form-control" id="provincia" name=Municipio_Nva> 
-            <option>Seleccione Municipio
-            </select> 
-            </div> 
-            <br>           
-          <div>
-                        
-NUEVO DATOS A RECOGER -->
-
-<!-- Direccion -->
-      <div class="form-group col-md-12">
-          <div class="form-group">
-            <label>Dirección</label>
-            <textarea rows="3" required="" name="Direccion_nva" type="text" class="form-control" id="Razon_nva" placeholder="Ingrese Dirección "></textarea>
-          </div>
-      </div>
- <!-- Correo -->   
- <div class="row">
-    <div class="col">      
-      <div class="form-group">         
-              <div class="form-group">
-              <label for="exampleInputPassword1">Correo</label>
-              <input required="" name="Correo_nva" type="email" class="form-control" id="Correo_nva" placeholder="Ingrese Correo ">
               </div>
-      </div>
-    </div>
+               <div class="row">
+                  <div class="col-md-12">
+                    <input type="file" name="">
+                  </div>
+               </div>
+              </div>
+              <div class="col-md-8">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Nit</label>
+                      <div class="row">
+                        <div class="col-xs-9 col-sm-9 col-md-9">
+                          <input required="" name="Nit_nva" type="text" class="form-control" id="Nit_nva" placeholder="Ingrese Nit">
+                        </div>
+                        <label>-</label>
+                        <div class="col-xs-2 col-sm-2 col-md-2">
+                          <input required="" name="Nit_nva_dig" type="text" class="form-control" id="Nit_nva_dig" placeholder="">
+                        </div>
+                      </div>
+                    </div>
+                  </div>        
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Razón</label>
+                      <input required="" name="Razon_nva" type="text" class="form-control" id="Razon_nva" placeholder="Ingrese Razón">
+                    </div>
+                  </div>
+                </div>
 
-<!-- Telefono -->
-    <div class="col">    
-        <div class="form-group col-md-12">
-            <div class="form-group">
-              <label for="exampleInputPassword1">Teléfono</label>
-              <input required="" name="Telefono_nva" type="text" class="form-control" id="Razon_nva" placeholder="Ingrese Teléfono ">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Departamento</label>                 
+                      <select  class="form-control" name=Departamento_nva id="Departamento_nva" onchange="municipios(this.value)">
+                        <option selected="" disabled="" value="">Seleccione Departamento</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Municipio</label>
+                        <select class="form-control" id="Municipio_nva" name=Municipio_nva > 
+                          <option selected="" disabled="" value="">Seleccione Municipio</option>
+                      </select> 
+                    </div> 
+                  </div>       
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Dirección</label>
+                      <textarea rows="3" required="" name="Direccion_nva" type="text" class="form-control" id="Razon_nva" placeholder="Ingrese Dirección "></textarea>
+                    </div>
+                  </div>
+                </div>
+              </div>         
+            </div>                 
+          <div>
+
+          <div class="row">
+            <div class="col">      
+              <div class="form-group">         
+                <div class="form-group">
+                <label for="exampleInputPassword1">Correo</label>
+                <input required="" name="Correo_nva" type="email" class="form-control" id="Correo_nva" placeholder="Ingrese Correo ">
+                </div>
+              </div>
             </div>
-        </div>
-    </div>
-</div> 
-<!-- FIN NUEVO DATOS A RECOGER  --> 
 
-         <div class="card-footer">
-            <button type="submit" class="btn btn-warning">Guardar</button>
-          </div>
-        </form>
+            <div class="col">    
+              <div class="form-group col-md-12">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Teléfono</label>
+                  <input required="" name="Telefono_nva" type="text" class="form-control" id="Razon_nva" placeholder="Ingrese Teléfono ">
+                </div>
+              </div>
+            </div>
+        </div> 
+
+        <div class="card-footer">
+          <button type="submit" class="btn btn-warning">Guardar</button>
+        </div>
+      </form>
     </div>
   </div>
+
+
+
+
+
   <hr>
   <div class="col-md-12">
    <div class="card card-warning">
@@ -254,7 +233,7 @@ NUEVO DATOS A RECOGER -->
     <div class="col">  
         <div class="form-group">
            <label for="exampleInputPassword1">Correo</label>
-          <input type="text" name="Correo_edit" id="Correo_edit" class="form-control" placeholder="Correo" value="{{ $EmpresasItem->Correo }}">   
+          <input type="email" name="Correo_edit" id="Correo_edit" class="form-control" placeholder="Correo" value="{{ $EmpresasItem->Correo }}">   
         </div> 
     </div>
         <br>
@@ -349,19 +328,18 @@ NUEVO DATOS A RECOGER -->
 
   <script src="plugins/toastr/toastr.min.js"></script>
 
+  <script type="text/javascript" src="js/colombia.json"></script>
+
   <script type="text/javascript">
 
 
-     $(function () {
+    $(function () {
+
       mensaje = $('#mensaje').html();
       tipo = $('#tipo').html();
       if (tipo=='Error') {toastr.error(mensaje)}
       if (tipo =='OK') {toastr.success(mensaje)}
          
-      
-
-       
-    
       $('#example2').DataTable({
         "paging": true,
         "lengthChange": false,
@@ -370,113 +348,34 @@ NUEVO DATOS A RECOGER -->
         "info": true,
         "autoWidth": true,
       });
+
+      //llenar select de departamento
+        $.getJSON("js/colombia.json", function(datos) {
+            $.each(datos, function(idx,id) {
+                $("#Departamento_nva").append(new Option(id.departamento, id.departamento));
+            });
+        });
     });
-  </script>
 
-<script>
-  var provincias_1=new Array("Leticia","Puerto Nariño");
-  var provincias_2=new Array(
-    "Medellín","Abejorral","Abriaquí","Alejandría","Amaga","Amalfi","Andes","Angelópolis","Angostura","Anorí","Anzá","Apartadó","Arboletes","Argelia","Armenia","Barbosa","Belmira","Bello","Betania","Betulia","Briceño","Buriticá","Cáceres","Caicedo","Caldas","Campamento","Cañasgordas","Caracolí","Caramanta","Carepa","Carolina del Príncipe","Caucasia","Chigorodó","Cisneros","Ciudad Bolívar","Cocorná","Concepción","Concordia","Copacabana","Dabeiba","Donmatías","Ebéjico","El Bagre","El Carmen de Viboral","El Jardín","El Jardín","El Peñol","El Retiro","El Santuario", "Entrerríos","Envigado","Fredonia","Frontino","Giraldo","Giradota","Gómez Plata","Granada","Guadalupe","Guarne","Guatapé","Heliconia","Hispania","Itagüí","Jericó","La Ceja","La Estrella","La Pintada","La Unión", "Liborina","Maceo","Marinilla","Montebello","Murindó","Mutatá","Nariño","Nechí","Necoclí","Olaya","Peque","Pueblorrico","Puerto Berrio","Puerto Nare","Puerto Triunfo","Remedios","Rionegro","Sabanalarga","Sabaneta","Salgar","San Andrés","San Carlos","San Francisco","San Jerónimo","San José de la Montaña","San Juan de Urabá","San Luis","San Pedro de los Milagros","San Pedro de Urabá","San Rafael", "San Roque", "San Vicente Ferrer","Santa Fe de Antioquia", "Santa Rosa de Osos", "Santo Domingo", "Segovia", "Sonsón","Sopetrán", "Támesis", "Tarazá", "Tarso", "Titiribí", "Toledo", "Turbo", "Uramita", "Urrao", "Valdivia", "Valparaíso", "Vegachí", "Venecia", "Vigía del Fuerte", "Yalí", "Yarumal", "Yolombó", "Yondó", "Zaragoza"
-    );
-  var provincias_3=new Array("Arauca","Arauquita","Cravo Norte","Fortul","Puerto Rondón","Saravena","Tame");
-  var provincias_4=new Array(
-    "Barranquilla","Baranoa","Campo de la Cruz","Candelaria","Galapa","Juan de Acosta","Luruaco","Malambo","Manatí","Palmar de Varela","Piojó","Polonuevo","Ponedera","Puerto Colombia","Repelón","Sabanagrande","Santa Lucía","Santo Tomás","Soledad","Suan","Tubará","Usiacurí"
-    );
-  var provincias_5=new Array(
-    "Cartagena de Indias","Achí","Altos del Rosario","Arenal","Arjona","Arroyohondo","Barranco de Loba","Calamar","Cantagallo","Cicuco","Clemencia","Córdoba","El Carmen de Bolívar","El Guamo","El Peñon","Hatillo de Loba","Magangué","Mahates","Margarita","María La Baja","Montecristo","Morales","Norosí","Pinillos","Regidor","Rioviejo","San Cristóbal","San Estanislao","San Fernando","San Jacinto","San Jacinto del Cauca","San Juan Nepomuceno","San Martín de Loba","San Pablo","Santa Catalina","Santa Cruz de Mompox","Santa Rosa","Santa Rosa del Sur","Simití","Soplaviento","Talaigua Nuevo","Tiquisio", "Turbaco","Turbana","Villanueva","Zambrano"
-    );
-  var provincias_6=new Array("-");
-  var provincias_7=new Array("-");
-  var provincias_8=new Array("-");
-  var provincias_9=new Array("-");
-  var provincias_10=new Array("-");
-  var provincias_11=new Array("-");
-  var provincias_12=new Array("-");
-  var provincias_13=new Array("-");
-  var provincias_14=new Array("-");
-  var provincias_15=new Array("-");
-  var provincias_16=new Array("-");
-  var provincias_17=new Array("-");
-  var provincias_18=new Array("-");
-  var provincias_19=new Array("-");
-  var provincias_20=new Array("-");
-  var provincias_21=new Array("-");
-  var provincias_22=new Array("-");
-  var provincias_23=new Array("-");
-  var provincias_24=new Array("-");
-  var provincias_25=new Array("-");
-  var provincias_26=new Array("-");
-  var provincias_27=new Array("-");
-  var provincias_28=new Array("-");
-  var provincias_29=new Array("-");
-  var provincias_30=new Array("-");
-  var provincias_31=new Array("-");
-  var provincias_32=new Array("-");
+    function municipios(nombre) {
+      //limpias el select
+        $('#Municipio_nva').empty().append('<option selected="selected" value="">Seleccione Municipio...</option>');
 
-  var todasProvincias = [
-    [],
-    provincias_1,
-    provincias_2,
-    provincias_3,
-    provincias_4,
-    provincias_5,
-    provincias_6,
-    provincias_7,
-    provincias_8,
-    provincias_9,
-    provincias_10,
-    provincias_11,
-    provincias_12,
-    provincias_13,
-    provincias_14,
-    provincias_15,
-    provincias_16,
-    provincias_17,
-    provincias_18,
-    provincias_19,
-    provincias_20,
-    provincias_21,
-    provincias_22,
-    provincias_23,
-    provincias_24,
-    provincias_25,
-    provincias_26,
-    provincias_27,
-    provincias_28,
-    provincias_29,
-    provincias_30,
-    provincias_31,
-    provincias_32,
-  ];
+      $.getJSON("js/colombia.json", function(datos) {
+          $.each(datos, function(idx,id) {
+              if (id.departamento==nombre) {
+                Total = id.ciudades.length
+                for (var i = 0; i < Total; i++) {
+                  $("#Municipio_nva").append(new Option(id.ciudades[i], id.ciudades[i]));
+                }
+              }
+          });
+      });
+    }
 
-  function cambia_provincia(){ 
-    //tomo el valor del select del pais elegido 
-    var pais 
-    pais = document.f1.pais[document.f1.pais.selectedIndex].value 
-    //miro a ver si el pais está definido 
-    if (pais != 0) { 
-        //si estaba definido, entonces coloco las opciones de la provincia correspondiente. 
-        //selecciono el array de provincia adecuado 
-        mis_provincias=todasProvincias[pais]
-        //calculo el numero de provincias 
-        num_provincias = mis_provincias.length 
-        //marco el número de provincias en el select 
-        document.f1.provincia.length = num_provincias 
-        //para cada provincia del array, la introduzco en el select 
-        for(i=0;i<num_provincias;i++){ 
-          document.f1.provincia.options[i].value=mis_provincias[i] 
-          document.f1.provincia.options[i].text=mis_provincias[i] 
-        } 
-    }else{ 
-        //si no había provincia seleccionada, elimino las provincias del select 
-        document.f1.provincia.length = 1 
-        //coloco un guión en la única opción que he dejado 
-        document.f1.provincia.options[0].value = "-" 
-        document.f1.provincia.options[0].text = "-" 
-    } 
-    //marco como seleccionada la opción primera de provincia 
-    document.f1.provincia.options[0].selected = true 
-}
+    function cambiar(){     
+      $("#btn_cam_img").click();
+    }
 
   </script>
 
